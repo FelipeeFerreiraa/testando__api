ded7faf85742aa99646831bdba7500bc37ec6a38
FelipeeFerreiraa <felipe333ferreira333@gmail.com>
2025-02-20 23:50:32 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,7 @@
-import { useState } from 'react'
-
 function Home() {
     return (
         <div>
-            <h1>Testando API</h1>
-            <Contador />
-            <div><br/>Felipe Ferreira Domingues</div>
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionaContador() {
-        setContador(contador + 1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionaContador}>Adicionar</button>
+            <h1>Felipe Ferreira Domingues</h1>
         </div>
     )
 }
